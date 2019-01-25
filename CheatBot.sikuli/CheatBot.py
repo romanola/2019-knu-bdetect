b6be0cbb4a2b6e92b120d494ec20073a3e006115
Roman Polishchenko <roma.vinn@gmail.com>
2019-01-25 14:41:36 +0200

    Updated bot.

--- CheatBot.sikuli/CheatBot.py
+++ CheatBot.sikuli/CheatBot.py
@@ -15,22 +15,16 @@ class Window:
         """
 
         # r = Region("""get your image here""")
-        r = Region(Region(6, 33, 1264, 615))
+        r = Region(Region(6,33,1264,615))
 
         self.dot = (r.x, r.y)
-        self.h = r.w
-        self.w = r.h
+        self.h = r.h
+        self.w = r.w
         self.center = (self.dot[0] + int(r.w / 2), self.dot[1] + int(r.h / 2))
 
         # angle between window diagonal and x axis
         self.angle = math.atan((self.dot[0] + self.w) / (self.dot[1] + self.h))
-
-    def shape(self):
-        """
-        :return: the four point tuple (defining window)
-        """
-        return self.dot, (self.dot[0] + self.w, self.dot[1]), (self.dot[0] + self.w, self.dot[1] + self.h), \
-               (self.dot[0], self.dot[1] + self.h)
+        self.diagonal = math.sqrt((self.dot[0] + self.w) ** 2 + (self.dot[1] + self.h) ** 2)
 
 
 class Runner:
@@ -51,6 +45,11 @@ class Runner:
         :param location: destination coordinates
         :return: None
         """
+        # doing a click
+        if needGo:
+            click(Location(*location))
+
+        # changing current position
         x, y = location  # in absolute display coordinates
         # in emulator window coordinates
         tmp_x = x - self.window.dot[0]
@@ -62,9 +61,7 @@ class Runner:
 
         self.current = (new_x, new_y)
 
-        if needGo:
-            click(Location(*location))
-
+        
     def _get_random_angle(self, deviation=5):
         """
         :param deviation: deviation
@@ -76,8 +73,7 @@ class Runner:
         """
         :return: random length
         """
-        diagonal = math.sqrt((self.window.dot[0] + self.window.w) ** 2 + (self.window.dot[1] + self.window.h) ** 2)
-        max_len = int(diagonal / 2)
+        max_len = self.window.diagonal // 2
         return rd.randrange(5, max_len - 10)
 
     def anotherWay(self):
@@ -192,10 +188,10 @@ def current_way(x_b, y_b, x_e, y_e):
     def f(x, y):
         try:
             coef = 15
-            return ((x - x_b) / (x_e - x_b) - (y - y_b) / (y_e - y_b) < 15 and
-                    (x - x_b) / (x_e - x_b) - (y - y_b) / (y_e - y_b) > -15 and
-                    x < x_e * math.copysign(1, x_e) + 3 and y < y_e * math.copysign(1, y_e) + 3 and
-                    x > x_b * math.copysign(1, x_b) and y > y_b * math.copysign(1, y_b))
+            return ((x - x_b) / (x_e - x_b) - (y - y_b) / (y_e - y_b) < coef and
+                    (x - x_b) / (x_e - x_b) - (y - y_b) / (y_e - y_b) > -coef and
+                    x < abs(x_e) + 3 and y < abs(y_e) + 3 and
+                    x > abs(x_b) and y > abs(y_b))
         except ZeroDivisionError:
             return None
 
@@ -239,15 +235,14 @@ def vector_transform(x_b, y_b, x_e, y_e, coef):
 
 
 def visitPoint(x, y):
-    a = -4
-    b = 4
-
+    
+    # random number of taps
     Taps = random.choice(fewTaps)
 
-    before = True
-    if random.randint(1, 5) % 2 == 0:
-        before = False
+    # random choice
+    before = random.choice(True, False)
 
+    # if the harvest is too close – make one tap    
     if distance(start.getX(), start.getY(), x, y) < 100:
         before = False
         Taps = 1
@@ -266,9 +261,9 @@ def visitPoint(x, y):
         runner.go((nx, ny))
         coef -= 0.1 if before else 0.3
 
-        time.sleep(random.randint(200, 400) / 1000)
+        # time.sleep(random.randint(200, 400) / 1000)
+        wait(GetDelay(nx, ny))
 
-    wait(GetDelay(x, y))
     LastPos = start.getX(), start.getY()
 
 
@@ -277,13 +272,36 @@ def checkObjects():
 
 
 def walkSomewhere():
-    upClosed = False
-    downClosed = False
-    rightClosed = False
-    leftClosed = False
+    """Run in a random way on a random distance."""
+    center_x = runner.window.center[1]
+    center_y = runner.window.center[0]
+    #center_x = SCREEN_RES_X // 2
+    #center_y = SCREEN_RES_Y // 2
+    #print('center_x', center_x)
+    #print('center_y', center_y)
+    diag = runner.window.diagonal
+    #print('diag', diag) 
+ 
+    # random distance
+    radius = diag // 7
+    radius += (random.random() - 0.5) * radius
+    #print('radius', radius)
+   
+    # random way
+    degree = random.random() * 2 * math.pi
+    #print('degree', degree)
+    sin_tmp = math.sin(degree)
+    cos_tmp = math.cos(degree)
+    new_x = center_x + cos_tmp * radius #* sign(cos_tmp)
+    new_y = center_y + sin_tmp * radius #* sign(sin_tmp)
+    #print('sin', sin_tmp)
+    #print('cos', cos_tmp)
+    
+    click(Location(new_x, new_y))
 
-    for i in range(5):
-        runner.anotherWay()
+    delay = GetDelay(new_x, new_y)
+    wait(delay)
+    #print(delay)
 
 
 # while exists(STATIC):
@@ -310,5 +328,4 @@ for i in range(50):
             visitArea(MA)
     else:
         walkSomewhere()
-
 
